d4289a614e31ecac6d808707d372b8316bc436ab
Shittu Emmanuel <shem4soul@gmail.com>
2026-03-06 20:35:11 +0100

    first commit

--- chapter two/server.js
+++ chapter two/server.js
@@ -5,4 +5,14 @@ const app = express();
 
 const PORT = 8383;
 
+app.get("/", (req, res) => {
+  console.log("Yay i hit an endpoint", req.method);
+  res.sendStatus(201);
+});
+
+app.get("/dashboard", (req, res) => {
+  console.log("Yay i hit the dashboard endpoint", req.method);
+  res.sendStatus(201);
+});
+
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
